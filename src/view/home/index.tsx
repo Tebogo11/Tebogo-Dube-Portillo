94fd0641acd6bba5dc764004e652af9c15858ea7
Tebogo <tebogo1106@live.co.uk>
2026-01-29 09:42:19 +0000

    fix mobile padding

--- src/view/home/index.tsx
+++ src/view/home/index.tsx
@@ -3,7 +3,7 @@ import RightSideSection from "./RightSideSection";
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col md:flex-row justify-center items-start md:min-h-dvh max-w-7xl mx-auto pt-20 px-10 overflow-hidden">
+    <div className="flex flex-col md:flex-row justify-center items-start md:min-h-dvh max-w-7xl mx-auto pt-20 px-5 md:px-10 overflow-hidden">
       <LeftSideSection />
       <RightSideSection />
     </div>
